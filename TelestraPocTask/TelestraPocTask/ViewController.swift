e19cf5acdb6e9bf882610881ed4cbea47e8a9d0f
Vishnu <v.r.ramachandran@accenture.com>
2019-12-23 17:16:26 +0530

    Created new project

---
+++ TelestraPocTask/TelestraPocTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TelestraPocTask
+//
+//  Created by v.r.ramachandran on 23/12/19.
+//  Copyright © 2019 v.r.ramachandran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
