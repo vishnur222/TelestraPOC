6cce1d5846883b405b7813b8a53d04d0be1d4038
Vishnu <v.r.ramachandran@accenture.com>
2019-12-23 18:31:32 +0530

    Added the following:
    1) New folder for Views.
    2) Added a file for UITableViewCell for custom cell.

---
+++ TelestraPocTask/TelestraPocTask/Views/TACountryCustomCellViewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TACountryCustomCellViewTableViewCell.swift
+//  TelestraPocTask
+//
+//  Created by v.r.ramachandran on 23/12/19.
+//  Copyright © 2019 v.r.ramachandran. All rights reserved.
+//
+
+import UIKit
+
+class TACountryCustomCellViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
