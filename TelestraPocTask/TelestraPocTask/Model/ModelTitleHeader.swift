8451e197f856abbe99317b4b63c95ede38fcf3dd
Vishnu <v.r.ramachandran@accenture.com>
2019-12-24 16:17:05 +0530

    Re-Indentation done to the class

--- TelestraPocTask/TelestraPocTask/Model/ModelTitleHeader.swift
+++ TelestraPocTask/TelestraPocTask/Model/ModelTitleHeader.swift
@@ -8,19 +8,19 @@
 
 import Foundation
 struct ModelTitleHeader : Codable {
-    let title : String?
-    let rows : [ModelTitleRow]?
-
-    enum CodingKeys: String, CodingKey {
-
-        case title = "title"
-        case rows = "rows"
-    }
-
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        title = try values.decodeIfPresent(String.self, forKey: .title)
-        rows = try values.decodeIfPresent([ModelTitleRow].self, forKey: .rows)
-    }
-
+  let title : String?
+  let rows : [ModelTitleRow]?
+  
+  enum CodingKeys: String, CodingKey {
+    
+    case title = "title"
+    case rows = "rows"
+  }
+  
+  init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+    title = try values.decodeIfPresent(String.self, forKey: .title)
+    rows = try values.decodeIfPresent([ModelTitleRow].self, forKey: .rows)
+  }
+  
 }
