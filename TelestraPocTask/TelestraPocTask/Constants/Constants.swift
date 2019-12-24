9e397a39e95a6a4e6e3684af4bcf2d8b97e62b47
Vishnu <v.r.ramachandran@accenture.com>
2019-12-24 16:15:11 +0530

    Added the title , ok button string in constant file

--- TelestraPocTask/TelestraPocTask/Constants/Constants.swift
+++ TelestraPocTask/TelestraPocTask/Constants/Constants.swift
@@ -10,24 +10,23 @@ import Foundation
 
 
 enum ApiTag: Int {
-    case getCountryDetails = 1000
+  case getCountryDetails = 1000
 }
 
 class Constants: NSObject {
+  
+  struct ConfigMessageValue {
+    static let initialEmptyMsg = "No Data available to display"
+    static let pullToRefreshMsg = "Pull to refresh"
+    static let noInternetMsg = "Please check your network conectivity"
+    static let decodingIssueMsg = "Decoding failure"
+    static let serverIssueMsg = "Not able to connect server try agian later"
+    static let alertOkBtn = "Ok"
+    static let alertTitle = "Title"
+  }
+  
+  struct ConfigdefaultImgName {
+    static let defaultImgName = "default"
     
-    struct ConfigMessageValue {
-        static let initialEmptyMsg = "No Data available to display"
-        static let pullToRefreshMsg = "Pull to refresh"
-        static let noInternetMsg = "Please check your network conectivity"
-        static let decodingIssueMsg = "Decoding failure"
-        static let serverIssueMsg = "Not able to connect server try agian later"
-        static let alertOkBtn = "Ok"
-        static let alertTitle = "Title"
-    }
-    
-    struct ConfigdefaultImgName {
-        static let defaultImgName = "default"
-        
-    }
+  }
 }
-
